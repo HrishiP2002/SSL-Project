075c11b483198b6dafc5d5e009643eb5303ec3c1
Eshita-pg <76476994+Eshita-pg@users.noreply.github.com>
2021-11-03 19:51:22 +0530

    Required fields

--- loginForm (1).php
+++ loginForm (1).php
@@ -12,17 +12,17 @@
         <p class="sign" align="center">Log in</p>
     <form action="loginVerify.php" method = "GET" class = "form1">
           <label for="User"></label><br>
-          <input type="text" id="User" name="User" class="un" placeholder="Username"><br>
+          <input type="text" id="User" name="User" class="un" placeholder="Username" required><br>
 
           <label for="Pass"></label><br>
-          <input type = "text" id = "Pass" name="Pass" class="pass" placeholder="Password"><br>
+          <input type = "text" id = "Pass" name="Pass" class="pass" placeholder="Password" required><br>
 
           <label for="car"> </label><br>
-          <input type = "text" id = "car" name="car" class="carno" placeholder="Car Number"><br>
+          <input type = "text" id = "car" name="car" class="carno" placeholder="Car Number" required><br>
 
           <!-- <input type="submit" value="Login"> -->
-          <a class="submit" type="submit" align="center">Login</a>
-        
+<!--           <a class="submit" type="submit" align="center">Login</a> -->
+        <button class="submit" type="submit" align="center" href="loginProceed.php" >Login</button>
     </form>
         </div>
     </body>
